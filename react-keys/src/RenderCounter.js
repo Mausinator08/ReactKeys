9a5cfa290d3f6a667281c044e8a28d70e52a8611
Mausinator08 <mauscomp@live.com>
2024-08-25 20:02:10 -0500

    Changed RenderComponent to a class and to add the componentDidMount() for instance counting.

--- react-keys/src/RenderCounter.js
+++ react-keys/src/RenderCounter.js
@@ -1,13 +1,32 @@
-let count = 0;
+import { Component } from "react";
 
-function RenderCounter(props) {
-	count++;
-	return (
-		<div>
-			<h2>{props.loggedIn ? "Logged In" : "Logged Out"}</h2>
-			<label>{count}</label>
-		</div>
-	);
+class RenderCounter extends Component {
+	props = {};
+	count = 0;
+
+	constructor(props) {
+		super(props);
+		this.props = props || {};
+		this.count = 0;
+	}
+
+	componentDidMount() {
+		this.props.increaseInstanceCount();
+	}
+
+	componentDidUnmount() {
+		this.props.decreaseInstanceCount();
+	}
+
+	render() {
+		this.count++;
+		return (
+			<div>
+				<h2>{this.props?.loggedIn ? "Logged In" : "Logged Out"}</h2>
+				<label>Renders: {this.count}</label>
+			</div>
+		);
+	}
 }
 
 export default RenderCounter;
